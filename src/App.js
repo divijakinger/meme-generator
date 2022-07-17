c6e6a70a3cbdc8510e42cf30bb50acb025911258
divijakinger <divija.kinger@somaiya.edu>
2022-07-17 14:37:34 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Meme from "./components/Meme";
+import Footer from "./components/Footer";
 
 export default function App(){
     return (
         <section>
             <Header/>
             <Meme/>
+            <Footer/>
         </section>
         
     )
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Footer(){
+    return (
+        <footer className="footer-sect">
+            <p>Made by <a href="https://github.com/divijakinger">Divija Kinger</a></p>
+        </footer>
+    )
+}
